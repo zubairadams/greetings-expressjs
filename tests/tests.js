101acae2316cd8d96a3b591c98985007d3e9de95
zubairadams <zubair.adams720@gmail.com>
2018-10-22 10:30:41 +0200

    heroku?

--- tests/tests.js
+++ tests/tests.js
@@ -2,12 +2,10 @@ let assert = require('assert');
 let greetings = require('../greetings-factory');
 
 describe('the greetings function', async function () {
-    it('should return the name greeted in the langauge chosen', async function () {
+    it('should return the name greeted in the langauge chosen', async function(){
         
         greetings.greet('zubair','english')
 
-
         assert.equal(hello , "zubair");
-    
     })
-    });
+});
