426871864fa1e68d2eca374d8e6e39d410fcd50a
zubairadams <zubair.adams720@gmail.com>
2018-10-31 16:22:47 +0200

    fixing tests

--- test/tests.js
+++ test/tests.js
@@ -2,7 +2,6 @@ let assert = require('assert');
 let greetings = require('../greetings-factory');
 const pg = require('pg');
 const Pool = pg.Pool;
-let indi = require('../index');
 let mocha = require("mocha")
 const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/greetings';
 
@@ -13,44 +12,50 @@ var ob = greetings(pool);
 
 describe('basic app testing', function () {
 
+    after(function() {
+        pool.end();
+    });
+
     beforeEach(async function () {
- 
         await pool.query("delete from usersGreeted;");
     });
-it('should greet a person in the language selected.', async function () {
 
-    // var ob = greetings(pool);
-    var greetings = await ob.greet('bud', 'english')
-    var greetings2 =  await ob.greet('cobus', 'afrikaans')
+    it('should greet a person in the language selected.', async function () {
 
-    assert.equal(greetings2,' More Cobus')
-    assert.equal(greetings,'Hello Bud')
-});
-it('should return Number of names greeted',async function() { 
+        // var ob = greetings(pool);
+        var greetings = await ob.greet('bud', 'english')
+        var greetings2 = await ob.greet('cobus', 'afrikaans')
 
-var number = {
- greetings : await ob.greet('bud', 'english'),
- greetings2 : await ob.greet('cobus', 'afrikaans')
-}
- var number2 = await ob.count(number)
- assert.equal(number2,2)
-})
-it("should return the error message",async function(){
- 
-    var error = {
-        greetings : await ob.greet('', ' '),
- greetings2 : await ob.greet('', '')
-    } 
- 
-})
-it("should reset all names in the data base ", async function(){
-    var number = {
-        greetings : await ob.greet('bud', 'english'),
-        greetings2 : await ob.greet('cobus', 'afrikaans')
-       }
-       var number2 = await ob.count(number);
-       var resets = await ob.resetBtn();
-       assert.equal(number2,2);
-       assert.equal(resets,)
+        assert.equal(greetings2, ' More Cobus')
+        assert.equal(greetings, 'Hello Bud')
+    });
+
+    it('should return Number of names greeted', async function () {
+
+        var number = {
+            greetings: await ob.greet('bud', 'english'),
+            greetings2: await ob.greet('cobus', 'afrikaans')
+        }
+        var number2 = await ob.count(number)
+        assert.equal(number2, 2)
+    })
+
+    it("should return the error message", async function () {
+
+        var error = {
+            greetings: await ob.greet('', ' '),
+            greetings2: await ob.greet('', '')
+        }
+
+    })
+    it("should reset all names in the data base ", async function () {
+        var number = {
+            greetings: await ob.greet('bud', 'english'),
+            greetings2: await ob.greet('cobus', 'afrikaans')
+        }
+        var number2 = await ob.count(number);
+        var resets = await ob.resetBtn();
+        assert.equal(number2, 2);
+        assert.equal(resets, )
     })
 });
